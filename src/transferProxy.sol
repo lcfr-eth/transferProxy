76ecb84c4b637e38988db7ac6acf8b2dc87119d3
lcfr <nobody@lcfr.io>
2023-05-29 02:11:35 -0400

    misc

--- src/transferProxy.sol
+++ src/transferProxy.sol
@@ -8,7 +8,7 @@ contract transferProxy {
 
     error notApproved();
 
-    function transfer(bytes[] calldata _data, address _contract, address _from) external {
+    function approvedCall(bytes[] calldata _data, address _contract, address _from) external {
         assembly {
             // check if caller isApprovedForAll() by _from on _contract or revert
             mstore(0x00, shl(224, 0xe985e9c5)) 
@@ -38,6 +38,43 @@ contract transferProxy {
                 calldatacopy(0x00, add(_data.offset, add(data, 0x20)), len)
                         
                 success := call( gas(), _contract, 0x00, 0x00, len, 0x00, 0x00)
+
+                if iszero(success) {
+                    returndatacopy(0x00, 0x00, returndatasize())
+                    revert(0x00, returndatasize())
+                }
+            }
+        }
+    }
+      function ownerDrop(uint256[] calldata tokenIds, address[] calldata _addrs, address _contract) external pure returns(bytes32) {
+        bytes4 transferFrom = 0x23b872dd;
+        assembly {
+
+            //if (iszero(eq(tokenIds.length, _sendToAddrs.length))) {
+            //     revert(0x00, 0x00)
+            // }
+
+            // let transferFrom := 0x23b872dd00000000000000000000000000000000000000000000000000000000
+            // let transferFrom := 0x23b872ddac1db17cac1db17cac1db17cac1db17cac1db17cac1db17cac1db17c
+
+            mstore(0x00, transferFrom) // transferFrom(address,address,uint256)
+            mstore(0x04, caller())
+
+             let i := 0
+             for {} 1 { i:= add(i, 1) } {
+                if eq(i, tokenIds.length){ break }
+
+                // offset for both arrays
+                let offset := add(_addrs.offset, shl(5, i))
+
+                // copy the address to send to
+                calldatacopy(0x24, add(_addrs.offset, offset), 0x20)
+
+                // copy the token id
+                calldatacopy(0x44, add(tokenIds.offset, offset), 0x20)
+                
+                // call transferFrom
+                success := call( gas(), _contract, 0x00, 0x00, 0x64, 0x00, 0x00)
 
                 if iszero(success) {
                     returndatacopy(0x00, 0x00, returndatasize())
--- test/transferProxy.t.sol
+++ test/transferProxy.t.sol
@@ -15,10 +15,16 @@ contract transferProxyTest is Test {
 
     function testTransfer() public {
         bytes[] memory data = new bytes[](1);
-        data[0] = "\x41\x42\x41\x42";
+        // data[0] = "\x41\x42\x41\x42";
         address _contract = 0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85;
         address _from = 0x10D0d49AeB4e06d5bbbE6Efe352aDb11045e89f7;
         address _caller = 0x1eAcA5cEc385A6C876D8A56f6c776Bb5857AcCbc;
-        proxy.transfer(data, _contract, _from, _caller);
+
+        vm.startPrank(_from);
+        vm.deal(_from, 1 ether);
+        
+        data[0] = abi.encodeWithSignature("transferFrom(address,address,uint256)", _from, _caller, 1);
+        proxy.approvedtransfer(data, _contract, _from, _caller);
+        vm.endPrank();
     }
 }
