db3e7bee3d124638b205e44761f9b0c474a90224
lcfr <me@lcfr.io>
2023-06-02 15:26:00 -0400

    deploy

---
+++ script/deploy.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "src/transferProxy.sol";
+import "forge-std/console.sol";
+
+contract DeployTransferProxyScript is Script {
+    function setUp() public {}
+
+    //run these commands to deploy to blockchain
+    //source .env
+    // forge script script/deploy.s.sol:DeployTransferProxyScript --private-key $DEPLOYER_PRIVATE_KEY --rpc-url $RPC_URL --broadcast -vv
+    function run() public {
+        vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
+        transferProxy TransferProxy = new transferProxy();
+        console.log("BasicBulkreg", address(TransferProxy));
+    }
+}
